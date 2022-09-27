e509d532747576e2ef4cbaf9057ec73b6fb8cdf5
daria_faskhieva <dashaf712@mail.ru>
2022-09-27 18:07:03 +0300

    проверка на ввод

--- PR_5_/MainWindow.xaml.cs
+++ PR_5_/MainWindow.xaml.cs
@@ -29,29 +29,130 @@ namespace PR_5_
             RBOrdinary.Visibility = Visibility.Visible;
             RBOriental.Visibility = Visibility.Collapsed;
             RBOrdinary.IsEnabled = true;
+            day.Text = "";
+            months.SelectedIndex = -1;
         }
         private void OrientCheck(object sender, RoutedEventArgs e) //выбран восточный гороскоп
         {
             RBOrdinary.Visibility = Visibility.Collapsed;
             RBOriental.Visibility = Visibility.Visible;
             RBOriental.IsEnabled = true;
+            year.Text = "";
         }
 
         private void months_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            switch (months.SelectedIndex)
+            {
+                case 0:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В январе 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 1:
+                    if (int.Parse(day.Text) > 28 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В феврале 28 дней. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 2:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В марте 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 3:
+                    if (int.Parse(day.Text) > 30 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В апреле 30 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 4:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В мае 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 5:
+                    if (int.Parse(day.Text) > 30 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В июне 30 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 6:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В июле 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 7:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В августе 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 8:
+                    if (int.Parse(day.Text) > 30 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В сентябре 30 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 9:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В октябре 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 10:
+                    if (int.Parse(day.Text) > 30 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В ноябре 30 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+                case 11:
+                    if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
+                    {
+                        MessageBox.Show("В декабре 31 день. Повторите ввод");
+                        day.Text = "";
+                    }
+                    break;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            switch (Convert.ToInt32(year.Text))
+            //if (day.Text != "" && months.Text != "")
+            //{
+
+            //}
+        }
+
+        private void day_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
             {
-                case 0:
-                    result.Text = "го";
-                    break;
+                e.Handled = true;
             }
+        }
 
-
+        private void year_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
