089f43eae3f0ad223c97523fd20608f7583ac706
daria_faskhieva <dashaf712@mail.ru>
2022-09-27 23:27:39 +0300

    восточный гороскоп

--- PR_5_/MainWindow.xaml.cs
+++ PR_5_/MainWindow.xaml.cs
@@ -27,22 +27,25 @@ namespace PR_5_
         private void OrdinCheck(object sender, RoutedEventArgs e) //выбран обычный гороскоп
         {
             RBOrdinary.Visibility = Visibility.Visible;
-            RBOriental.Visibility = Visibility.Collapsed;
+            SPOrdinary.Visibility = Visibility.Visible;
+            spBtn.Visibility = Visibility.Visible;
+            SPOriental.Visibility = Visibility.Collapsed;
             RBOrdinary.IsEnabled = true;
-            day.Text = "";
-            months.SelectedIndex = -1;
+            result.Text = "";
         }
         private void OrientCheck(object sender, RoutedEventArgs e) //выбран восточный гороскоп
         {
-            RBOrdinary.Visibility = Visibility.Collapsed;
             RBOriental.Visibility = Visibility.Visible;
+            SPOriental.Visibility = Visibility.Visible;
+            spBtn.Visibility = Visibility.Visible;
+            SPOrdinary.Visibility = Visibility.Collapsed;
             RBOriental.IsEnabled = true;
-            year.Text = "";
+            result.Text = "";
         }
 
-        private void months_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void CBMonths_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch (months.SelectedIndex)
+            switch (CBMonths.SelectedIndex)
             {
                 case 0:
                     if (int.Parse(day.Text) > 31 || int.Parse(day.Text) < 1)
@@ -133,10 +136,81 @@ namespace PR_5_
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //if (day.Text != "" && months.Text != "")
-            //{
+            try
+            {
+                if (RBOrdinary.IsChecked == true)
+                {
+                    switch (CBMonths.SelectedIndex)
+                    {
+                        case 0:
+                            if (((Convert.ToInt32(day.Text) >= 21) && (Convert.ToInt32(day.Text) <= 31) && CBMonths.SelectedIndex == 2) || ((Convert.ToInt32(day.Text)>=1)&&(Convert.ToInt32(day.Text)<=20)&&CBMonths.SelectedIndex==3))
+                            {
+                                result.Text = "Ваш знак зодиака Овен";
 
-            //}
+                            }
+                            break;
+                    }
+                }
+                else if(RBOriental.IsChecked == true)
+                {
+                    if (Convert.ToInt32(year.Text) % 12 == 0)
+                    {
+                        result.Text = "По восточному гороскопу Вы - обезьяна";
+                    }
+                    else if(Convert.ToInt32(year.Text) % 12 == 1)
+                    {
+                        result.Text = "По восточному гороскопу Вы - петух";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 2)
+                    {
+                        result.Text = "По восточному гороскопу Вы - собака";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 3)
+                    {
+                        result.Text = "По восточному гороскопу Вы - свинья";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 4)
+                    {
+                        result.Text = "По восточному гороскопу Вы - крыса";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 5)
+                    {
+                        result.Text = "По восточному гороскопу Вы - бык";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 6)
+                    {
+                        result.Text = "По восточному гороскопу Вы - тигр";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 7)
+                    {
+                        result.Text = "По восточному гороскопу Вы - кролик";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 8)
+                    {
+                        result.Text = "По восточному гороскопу Вы - дракон";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 9)
+                    {
+                        result.Text = "По восточному гороскопу Вы - змея";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 10)
+                    {
+                        result.Text = "По восточному гороскопу Вы - лошадь";
+                    }
+                    else if (Convert.ToInt32(year.Text) % 12 == 11)
+                    {
+                        result.Text = "По восточному гороскопу Вы - коза";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Что-то не так");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так.Ошибка");
+            }
         }
 
         private void day_PreviewTextInput(object sender, TextCompositionEventArgs e)
